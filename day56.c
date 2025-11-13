0be0aa08337488aeda6f570f3c9bda4acd26af82
Kudrat28 <kudratsingh1322@gmail.com>
2025-11-13 09:55:05 +0530

    Add files via upload

---
+++ day56.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("enter size of array : ");
+    scanf("%d",&n);
+
+    int arr[n];
+
+    printf("Enter the elements : ");
+    for (int i=0; i<n; i++) {
+        scanf("%d" , &arr[i]);
+    }
+
+    for(int i=0; i<n; i++) {
+        int next = -1;
+        for(int j=i+1; j<n; j++) {
+            if (arr[j] > arr[i]) {
+            next = arr[j];
+            break;
+        }
+    }
+
+    printf("%d", next);
+    if (i != n-1) printf(", ");
+
+}
+return 0;
+}
+
+/*enter size of array : 4
+Enter the elements : 1 3 2 4
+3, 4, 4, -1
+*/  
---
+++ day57.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("enter size of array : ");
+    scanf("%d",&n);
+
+    int arr[n];
+
+    printf("Enter the elements : ");
+    for (int i=0; i<n; i++) {
+        scanf("%d" , &arr[i]);
+    }
+
+    for(int i=0; i<n; i++) {
+        int prev = -1;
+        for(int j=i-1; j>=0; j--) {
+            if (arr[j] > arr[i]) {
+            prev = arr[j];
+            break;
+        }
+    }
+
+    printf("%d", prev);
+    if (i != n-1) printf(", ");
+
+}
+return 0;
+}
+
+/*enter size of array : 4
+Enter the elements : 1 3 2 4
+-1, -1, 3, -1
+*/
+
+    
